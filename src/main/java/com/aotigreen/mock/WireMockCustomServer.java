08d4210e8814a3f948f8220a81d84a3505cc6564
NAN HU <nancy@nancy.local>
2018-10-19 12:38:56 +0800

    为啥igonre文件已经加过了，还是会提交很多无用的文件

--- src/main/java/com/aotigreen/mock/WireMockCustomServer.java
+++ src/main/java/com/aotigreen/mock/WireMockCustomServer.java
@@ -4,6 +4,8 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.http.trafficlistener.ConsoleNotifyingWiremockNetworkTrafficListener;
+import com.github.tomakehurst.wiremock.recording.RecordSpecBuilder;
+import org.wiremock.webhooks.Webhooks;
 
 /**
  * nancy.hu
@@ -15,6 +17,7 @@ public class WireMockCustomServer {
 
         //No-args constructor will start on port 8080, no HTTPS
         WireMockConfiguration config = new WireMockConfiguration().port(8089);
+
         //TODO 关闭回放功能
         config.disableRequestJournal();
         //注册自定义response转换器
@@ -22,6 +25,9 @@ public class WireMockCustomServer {
         config.extensions(CustomRespTransfer1.class);
         //注册 通用response 模版   可以和ResponseTransfer配合使用，但是不可以和ResponeDefintionTransfer配置使用
         config.extensions(new ResponseTemplateTransformer(false));
+
+        //注册webhook，调用异步回调的功能
+        config.extensions(new Webhooks());
 
         //mapping和file文件夹所在位置
         config.usingFilesUnderClasspath("./");
@@ -31,7 +37,11 @@ public class WireMockCustomServer {
 
         config.networkTrafficListener();
         WireMockServer wireMockServer = new WireMockServer(config);
+//        wireMockServer.snapshotRecord(
+//                new RecordSpecBuilder().makeStubsPersistent(true));
         wireMockServer.start();
+//        wireMockServer.startRecording("http://localhost:8080/callback");
+
 
         //http://localhost:8089/local-transform4/
         //http://localhost:8089/__admin/docs/
